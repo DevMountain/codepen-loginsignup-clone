64ab8f9739533f10b6b860300643d0e89ac96a8c
Cahlan Sharp <cahlan@gmail.com>
2013-10-14 10:47:27 -0600

    Adds possible solution code/markup

--- script.js
+++ script.js
@@ -31,6 +31,70 @@ Reponse Objects:
 
 $('document').ready(function() {
 
-	//put code here
+	codepen.objects.User = {
+		name: null,
+		email: null,
+		username: null,
+		password: null,
+		is_logged_in: false
+	}
+
+	var NewUser = Object.create(codepen.objects.User, {
+		firstname: {
+			writable: true, 
+			enumerable: true,
+			value: ''
+		},
+		lastname: {
+			writable: true, 
+			enumerable: true,
+			value: ''
+		}
+	});
+
+	$('.signup-form').hide();
+
+	$('.login-form-btn').click(function() {
+		$(this).addClass('active');
+		$('.signup-btn').removeClass('active');
+		$('.signup-form').hide();
+		$('.login-form').show();
+	});
+	$('.signup-form-btn').click(function() {
+		$(this).addClass('active');
+		$('.login-btn').removeClass('active');
+		$('.signup-form').show();
+		$('.login-form').hide();
+	});
+
+	$('.btn-login').click(function() {
+		var user = Object.create(NewUser);
+		user.username = $('#login-username-field').val();
+		user.password = $('#login-password-field').val();
+		var response = codepen.api.login(user);
+		if(response.success == true) {
+			$('.login-form .form-feedback').html("Your'e logged in!");
+			user.is_logged_in = true;
+		}
+		else {
+			$('.login-form .form-feedback').html("Sorry, login didn't work");
+		}
+	});
+
+	$('.btn-signup').click(function() {
+		var user = Object.create(NewUser);
+		user.firstname = $('#signup-firstname-field').val();
+		user.lastname = $('#signup-lastname-field').val();
+		user.email = $('#signup-email-field').val();
+		user.username = $('#signup-username-field').val();
+		user.password = $('#signup-password-field').val();
+		var response = codepen.api.signup(user);
+		if(response.success == true) {
+			$('.signup-form .form-feedback').html("Your'e signed up! Now go login.");
+		}
+		else {
+			$('.signup-form .form-feedback').html(response.error);
+		}
+	});
 
 });
